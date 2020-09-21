801b5f150d49a090314d2ec969827f4bd8090898
xfei <xiang.fei@eisgroup.com>
2020-09-21 17:21:36 +0800

    add annotation

--- microservice-consumer-movie-ribbon/src/main/java/com/study/consumer/ConsumerRibbonApplication.java
+++ microservice-consumer-movie-ribbon/src/main/java/com/study/consumer/ConsumerRibbonApplication.java
@@ -1,14 +1,17 @@
 package com.study.consumer;
 
+import com.study.config.TestConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "microservice-producer-user", configuration = TestConfiguration.class)
 public class ConsumerRibbonApplication {
 
 	@Bean
