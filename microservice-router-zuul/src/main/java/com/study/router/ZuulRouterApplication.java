6c9b8e774a86ad455dcf890c8741783394ec57f3
xfei <xiang.fei@eisgroup.com>
2020-09-23 17:54:04 +0800

    Add router zuul

---
+++ microservice-router-zuul/src/main/java/com/study/router/ZuulRouterApplication.java
@@ -0,0 +1,17 @@
+package com.study.router;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class ZuulRouterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulRouterApplication.class, args);
+	}
+
+}
