d1945307e7560e5caf59596dfe8760176ea0d1fa
xfei <xiang.fei@eisgroup.com>
2020-09-24 15:38:36 +0800

    Add spring cloud configuration center

--- microservice-consumer-movie-ribbon/src/main/java/com/study/consumer/controller/MovieController.java
+++ microservice-consumer-movie-ribbon/src/main/java/com/study/consumer/controller/MovieController.java
@@ -3,6 +3,7 @@ package com.study.consumer.controller;
 import com.study.consumer.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,13 +14,22 @@ import org.springframework.web.client.RestTemplate;
  * @Date: 2020/9/16 14:36
  */
 @RestController
+@RefreshScope
 public class MovieController {
 
     @Autowired
     private RestTemplate restTemplate;
 
+    @Value("${hello.cloud}")
+    private String testConfigCenter;
+
     @GetMapping("/movie/{id}")
     public User findById(@PathVariable Long id) {
         return restTemplate.getForObject("http://microservice-producer-user/simple/" + id, User.class);
+    }
+
+    @GetMapping("/getConfigInfo")
+    public String getConfigInfo() {
+        return this.testConfigCenter;
     }
 }
---
+++ microservice-server-config/src/main/java/com/study/config/ConfigurationApplication.java
@@ -0,0 +1,17 @@
+package com.study.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class ConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurationApplication.class, args);
+	}
+
+}
